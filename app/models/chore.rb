4bef85138ac709a1e1f9a3b9d37404f1d34bf637
Evan Byrd <ebyrd@andrew.cmu.edu>
2018-05-17 16:38:11 -0400

    chore model

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,21 @@
-class Chore < ApplicationRecord
+class Chore < ActiveRecord::Base
 	belongs_to :child
 	belongs_to :task
+
+	validates_date :due_on
+
+	scope :by_task, -> { joins(:task).order('tasks.name') }
+	scope :chronological, -> { order(:due_on) }
+	scope :pending, -> { where(completed: false) }
+	scope :done, -> { where(completed: true) }
+	scope :past, -> { where('due_on < ?', Date.today) }
+	scope :upcoming, -> { where('due_on >= ?', Date.today) }
+
+	def status
+		if completed?
+			return 'Completed'
+		else
+			return 'Pending'
+		end	
+	end
 end
